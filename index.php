e748a52bb73abde858698512d495deda5b9602fe
Anthony Fletcher <afletcher2433.com>
2023-11-11 16:37:20 -0500

    New Commit

--- index.php
+++ index.php
@@ -60,3 +60,23 @@ echo "The answer is $divideAns\n"
 
 
 ?>
+
+<?php
+
+function factorial($n) {   //defining func that takes 1 param
+    if ($n < 0) {           //if statement to check if param is less than 0
+        return "Factorial is not defined for negative numbers";    //if cond is true then return if undef for neg nums
+    } elseif ($n == 0 || $n == 1) {    //if cond above is F, check if param is equal to 0 || 1
+        return 1;   //if above is true this line returns 1
+    } else {   //if no cond above are met
+        $result = 1;    //initializes var to 1 
+        for ($i = 2; $i <= $n; $i++) {    /*initialize var to 2, cont if $i is <= 1,
+                                             each iteration $i is incremented by 1 */ 
+            $result *= $i;   //multp curr val of $result by curr val $i and assigns back to $result
+        }
+        return $result;  //returns final value of $result which reps factorial input of $n
+    }
+}
+
+$number = 3; 
+echo "The factorial of $number is " . factorial($number); 
